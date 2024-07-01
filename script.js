137110c2b261f4165d0ef5d3f0bb1fe1280dd6ff
devnurislam <nurulislamgazi2@gmail.com>
2024-07-01 15:47:31 +0600

    a minor change

--- script.js
+++ script.js
@@ -13,32 +13,22 @@ document.addEventListener('DOMContentLoaded', () => {
     function showNotification() {
         if (Notification.permission === 'granted') {
             new Notification('Reminder', {
-                body: 'Say Alhamdulillah!',
+                body: 'Say & Feel Alhamdulillah!',
             });
         }
     }
 
     setInterval(showNotification, 4 * 60 * 1000); // 4 minutes in milliseconds
-});
-document.addEventListener('DOMContentLoaded', () => {
-    if (!('Notification' in window)) {
-        alert('This browser does not support desktop notification');
-        return;
-    }
-
-    Notification.requestPermission().then(permission => {
-        if (permission !== 'granted') {
-            alert('Permission for notifications was denied');
-        }
-    });
 
     function showNotification() {
         if (Notification.permission === 'granted') {
             new Notification('Reminder', {
-                body: 'Say astagfirullahh!',
+                body: 'Say & Feel Astagfirullahh!',
             });
         }
     }
 
     setInterval(showNotification, 7 * 60 * 1000); // 7 minutes in milliseconds
+
 });
+
